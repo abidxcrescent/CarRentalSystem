ae9c77d25773efe405230e68b8c35732428afb35
Abid Ali <mohdabidaliusa@gmail.com>
2025-05-12 12:39:47 -0700

    Initial commit

---
+++ vehicle/Vehicle.java
@@ -0,0 +1,124 @@
+package vehicle;
+
+import java.util.Date;
+
+public class Vehicle {
+
+    private int vehicleID;
+    private String licensePlate;
+    private VehicleType vehicleType;
+    private String companyName;
+    private String modelName;
+    private int kmDriven;
+    private Date manufactureDate;
+    private int fuelConsumption;
+    private int enginePower;
+    private int dailyRentalCost;
+    private int hourlyRentalCost;
+    private int noOfSeats;
+    private Status status;
+
+    public int getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(int vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getKmDriven() {
+        return kmDriven;
+    }
+
+    public void setKmDriven(int kmDriven) {
+        this.kmDriven = kmDriven;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public int getDailyRentalCost() {
+        return dailyRentalCost;
+    }
+
+    public void setDailyRentalCost(int dailyRentalCost) {
+        this.dailyRentalCost = dailyRentalCost;
+    }
+
+    public int getHourlyRentalCost() {
+        return hourlyRentalCost;
+    }
+
+    public void setHourlyRentalCost(int hourlyRentalCost) {
+        this.hourlyRentalCost = hourlyRentalCost;
+    }
+
+    public int getNoOfSeats() {
+        return noOfSeats;
+    }
+
+    public void setNoOfSeats(int noOfSeats) {
+        this.noOfSeats = noOfSeats;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
---
+++ vehicle/inventory/Store.java
@@ -0,0 +1,4 @@
+package vehicle.inventory;
+
+public class Store {
+}
